d98c75d8ea52eddb9c10b072bda5875a38b44f8e
wangweixin <w17731138318@126.com>
2018-08-17 16:15:32 +0800

    add CustomFont

--- CustomFont/CustomFont/ViewController.swift
+++ CustomFont/CustomFont/ViewController.swift
@@ -2,24 +2,80 @@
 //  ViewController.swift
 //  CustomFont
 //
-//  Created by 王伟欣 on 2018/8/17.
-//  Copyright © 2018年 王伟欣. All rights reserved.
+//  Created by Allen on 16/1/7.
+//  Copyright © 2016年 Allen. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var data = ["30 Days Swift", "这些字体特别适合打「奋斗」和「理想」", "谢谢「造字工房」，本案例不涉及商业使用", "使用到造字工房劲黑体，致黑体，童心体", "呵呵，再见🤗 See you next Project", "微博 @Allen朝辉",
+                "测试测试测试测试测试测试",
+                "123",
+                "Alex",
+                "@@@@@@"]
+    
+    var fontNames = ["MFTongXin_Noncommercial-Regular", "MFJinHei_Noncommercial-Regular", "MFZhiHei_Noncommercial-Regular", "Zapfino", "Gaspar Regular"]
+    
+    var fontRowIndex = 0
+    
+    @IBOutlet weak var changeFontButton: UIButton!
+    @IBOutlet weak var fontTableView: UITableView!
+    @IBAction func changeFontDidTouch(_ sender: AnyObject) {
+        
+        fontRowIndex = (fontRowIndex + 1) % 5
+        print(fontNames[fontRowIndex])
+        fontTableView.reloadData()
+        
+    }
+    
+    override var preferredStatusBarStyle : UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        fontTableView.dataSource = self
+        fontTableView.delegate = self
+        
+        //        for family in UIFont.familyNames {
+        //            for font in UIFont.fontNames(forFamilyName: family){
+        //                print(font)
+        //            }
+        //        }
+        
+        changeFontButton.layer.cornerRadius = 50
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 35
     }
-
-
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = fontTableView.dequeueReusableCell(withIdentifier: "FontCell", for: indexPath)
+        
+        let text = data[indexPath.row]
+        
+        cell.textLabel?.text = text
+        cell.textLabel?.textColor = UIColor.white
+        cell.textLabel?.font = UIFont(name:self.fontNames[fontRowIndex], size:16)
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
 }
 
